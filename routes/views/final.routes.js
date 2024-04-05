76155d303b8b65fa3201c549107fd2657591c7b4
Anastasiya Kesyan <kesyan_anastasiya@mail.ru>
2024-04-05 10:39:57 +0300

    final page

---
+++ routes/views/final.routes.js
@@ -0,0 +1,16 @@
+const router = require('express').Router()
+const FinalPage = require('../../components/pages/FinalPage');
+const { User } = require('../../db/models')
+
+router.get('/', async (req, res) => {
+    // const { user } = res.app.locals;
+    try {
+        const user = await User.findOne({ where: { id: 1 } })
+        const html = res.renderComponent(FinalPage, { title: 'final page', user })
+        res.send(html)
+    } catch ({ message }) {
+        res.status(500).json({ message })
+    }
+})
+
+module.exports = router;
