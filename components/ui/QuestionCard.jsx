740919bab90b0628ca0134b95173a4dc6f839d71
Anastasiya Kesyan <kesyan_anastasiya@mail.ru>
2024-04-05 15:30:43 +0300

    fetch power

--- components/ui/QuestionCard.jsx
+++ components/ui/QuestionCard.jsx
@@ -2,16 +2,16 @@ const React = require("react");
 
 module.exports = function QuestionCard({ question }) {
   return (
-    <div className="questionCard" >
+    <div className="questionCard">
       <h2>122131231231</h2>
       <img src={question.img} alt="" />
       <h2>{question.description}</h2>
       <form className="answerForm" data-questionId={question.id}>
         {" "}
-        <input type='text' name='answer' placeholder='Введите ответ' required />
-      <button className='OKbut' type='submit'>
-        Ответить
-      </button>
+        <input type="text" name="answer" placeholder="Введите ответ" required />
+        <button className="OKbut" type="submit">
+          Ответить
+        </button>
       </form>
     </div>
   );
