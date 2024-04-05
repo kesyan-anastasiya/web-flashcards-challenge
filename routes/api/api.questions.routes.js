d089c7f02d01774f891115edfce89a8ece544087
Anastasiya Kesyan <kesyan_anastasiya@mail.ru>
2024-04-05 16:42:57 +0300

    final

--- routes/api/api.questions.routes.js
+++ routes/api/api.questions.routes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { Question } = require('../../db/models')
+const { Question, User } = require('../../db/models')
 
 router.post('/', async (req, res) => {
     try {
@@ -9,15 +9,15 @@ router.post('/', async (req, res) => {
         } else {
             const { user } = res.app.locals;
             const question = await Question.findOne({ where: { id: questionId } })
-        
-            if (question.answer === answer) {
-                res.status(200).json({ message: "Умница! Мама с папой гордятся тобой!"})
+            if (question.answer.toLowerCase() === answer.toLowerCase()) {
+                res.app.locals.user.score += 100;
+                await User.update({ score: res.app.locals.user.score }, { where: { id: res.app.locals.user.id } })
+                res.status(200).json({ message: "Умница! Мама с папой гордятся тобой!" })
             } else {
-                res.status(400).json({ message: `Не, там все не так, а так "${question.answer}"`})
+                res.status(400).json({ message: `Не, там все не так, а так "${question.answer}"` })
             }
         }
     } catch ({ message }) {
-    
         res.status(500).json({ message })
     }
 })
